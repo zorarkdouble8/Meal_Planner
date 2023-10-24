e9793debf6c0d729e0ae019293a11954890da9ee
zorarkdouble8 <zorarkdouble8@gmail.com>
2023-10-24 05:55:19 -0400

    Made new data viewer class and cleaned up code

--- Application/window.py
+++ Application/window.py
@@ -3,6 +3,7 @@ import tkinter
 from tkinter import ttk
 
 import database
+from window_classes import *
 
 def main():
     global __root__
@@ -43,62 +44,9 @@ def main():
 
     add_meal_frame()
 
-    #Configures the widgets in the data frame
-    def config_data_frame():
-        #Where meals will be added
-        database_frame = ttk.Frame(tabs, relief="ridge", style="G.TFrame")
-        tabs.add(database_frame, text="Meal Directory")
-        database_frame.columnconfigure(0, weight=1)
-        database_frame.rowconfigure(0, weight=1)
+    meal_editor = TableEditorFrame(__root__, tabs, "Meals", "meal")
+    tabs.add(meal_editor.frame, text="Meal Directory")
 
-        #adds the meal data into the table
-        def config_data_table():
-            global data_viewer
-            meal_columns = database.get_table_columns("Meals")
-
-            data_viewer= ttk.Treeview(database_frame, show="headings", columns=[index for index, header in enumerate(meal_columns)])
-            data_viewer.grid(column=0, row=0, rowspan=5, sticky="NSEW")
-            data_viewer.columnconfigure(0, weight=1)
-            data_viewer.rowconfigure(0, weight=1)
-
-        
-            for index, column in enumerate(meal_columns):
-                data_viewer.heading(index, text=f"{column[0]}")
-
-            mealData = database.get_all_table_data("Meals") #TODO add if it's false *Error
-            
-            for row in mealData:
-                data_viewer.insert('', tkinter.END, values=row) 
-
-        #Adds the options to delete, add, and so on
-        def config_options():
-            options_frame = ttk.Frame(database_frame)
-            options_frame.grid(column=1, row=0, sticky="N")
-
-            info_label = ttk.Label(options_frame, text="Options")
-            info_label.grid(column=0, row=0)
-
-            delete_button = ttk.Button(options_frame, text="Delete Row")
-            delete_button.grid(column=0, row=3)
-            delete_button.bind("<ButtonPress-1>", lambda e: delete_row_from_database_viewer())
-
-            edit_button = ttk.Button(options_frame, text="Edit Row")
-            edit_button.grid(column=0, row=2)
-            edit_button.bind("<ButtonPress-1>", lambda e: edit_row_from_database_viewer())
-
-            add_button = ttk.Button(options_frame, text="Add Meal")
-            add_button.grid(column=0, row=1)
-            add_button.bind("<ButtonPress-1>", lambda e: meal_window_editor()) 
-
-            save_button = ttk.Button(options_frame, text="Save")
-            save_button.grid(column=0, row=4)
-            save_button.bind("<ButtonPress-1>", lambda e: database.save_data())
-
-        config_data_table()
-        config_options()
-
-    #TODO I would like these methods to be up on top of the functions
-    config_data_frame()  
 
     def add_settings_frame(): #TODO fully implement meal frame
         settings_frame = ttk.Frame(tabs)
@@ -108,19 +56,6 @@ def main():
     add_settings_frame()
 
     __root__.mainloop()
-
-#Gets the row to be edditted and shows the meal window editor to edit the row
-def edit_row_from_database_viewer():
-    row_info = data_viewer.item(data_viewer.focus())
-    meal_window_editor(row_info["values"])
-   
-#Deletes a selected row from the database viewer
-def delete_row_from_database_viewer():
-    try:
-        row_selection = data_viewer.focus()
-        data_viewer.delete(row_selection)
-    except:
-        show_error_window("No row selected!")
 
 #Shows a error screen and takes in a argument error (that shows in text in the window)
 def show_error_window(error):
@@ -132,74 +67,6 @@ def show_error_window(error):
 
     label = ttk.Label(window, text=f"{error}")
     label.grid(column=0, row=0)
-
-#Shows a window to add meals to the database, requires the root of the window and the data viewer for the meal table
-def meal_window_editor(meal_info = None):
-    window = tkinter.Toplevel()
-
-    if (meal_info == None or len(meal_info) == 0):
-        window.title("Add a meal")
-    else:
-        window.title("Edit a meal")
-
-    window.minsize(window.winfo_reqwidth() + 50, window.winfo_reqwidth() + 50)
-    #Centers and sizes window (width x height + xPos + yPos)
-    window.geometry(f"200x200+{math.floor(__root__.winfo_screenwidth()/2 - 100)}+{math.floor(__root__.winfo_screenheight()/2 - 100)}")
-
-    meal_columns = database.get_table_columns("Meals")
-
-    entries = []
-    labels = []
-
-    for index, column in enumerate(meal_columns): #TODO have entries be type based on the column
-        column_name = column[0]
-        type = column[1]
-
-        label = ttk.Label(window, text=f'{column_name}:')
-        label.grid(column=0, row=index)
-
-        if (type == "TEXT"):
-            entry = ttk.Entry(window)
-            entry.grid(column=1, row=index)
-            entries.append(entry)
-            labels.append(label)
-        elif (column_name == "ID"):
-            id_label = ttk.Label(window, text="Automatically Generated")
-            id_label.grid(column=1, row=index)
-
-    if (meal_info == None or len(meal_info) == 0):
-        add_button = ttk.Button(window, text="Add Meal")
-        add_button.bind("<ButtonPress-1>", lambda e: (add_meal(labels, entries), window.destroy()))
-    else:
-        add_button = ttk.Button(window, text="Save Meal")
-        add_button.bind("<ButtonPress-1>", lambda e: (edit_meal_database(entries), window.destroy()))
-
-    add_button.grid(column=1, sticky="S")
-    
-def edit_meal_database(*args): 
-    pass #TODO solve index problem
-
-def add_meal(labels, entries): #TODO error catching
-    """Adds a meal to the database and then refreshes the database viewer
-    
-    Arguements: labels (the label associated with the entry)
-                entries (entries used for user input)
-    """
-    meal = []
-
-    for index, entry in enumerate(entries):
-        text = entry.get()
-        if (text == ''):
-            text = "None"
-
-        meal.append((labels[index].cget("text").replace(":", ""), text))
-
-    database.add_row_to_table("Meals", meal)
-    add_meal_to_database_viewer(meal)
-
-#refreshes the database viewer widget
-def add_meal_to_database_viewer(meal):
-    data_viewer.insert('', tkinter.END, values=meal)
 
 if (__name__ == "__main__"):
     main()
---
+++ Application/window_classes.py
@@ -0,0 +1,159 @@
+import math
+from tkinter import Tk
+from tkinter import ttk
+import tkinter
+
+import database
+
+class TableEditorFrame():
+    #TODO customize button names and such
+
+    def __init__(self, root, parent, table_name, object_message):
+        """Adds a table editor frame to a parent obj
+        
+        Arguements: root (the root of the application)
+                    parent (the parent this frame will attach to)
+                    table_name (the table this editor will show)
+                    object_message (a string that customizes the option message IE: add a 'meal', add a <object_message>)
+        """
+        self.root = root
+        self.table_name = table_name
+        self.object_message = object_message.lower()
+
+        self.frame = ttk.Frame(parent)
+        self.options_frame = ttk.Frame(self.frame)
+        self.options_frame.grid(column=1, row=0, sticky="N")
+
+        try:
+            self.columns = database.get_table_columns(table_name)
+            self.data = database.get_all_table_data(table_name)
+        except Exception as error: #TODO customize error
+            raise Exception(error)
+        
+        self.__configure_data_viewer__()
+        self.__configure_options__()
+
+    def __configure_data_viewer__(self):
+        self.data_viewer = ttk.Treeview(self.frame, show="headings", columns=self.columns)
+        self.data_viewer.grid(column=0, row=0, rowspan=5, sticky="NSEW")
+        self.data_viewer.columnconfigure(0, weight=1)
+        self.data_viewer.rowconfigure(0, weight=1)
+
+        for index, column in enumerate(self.columns):
+            self.data_viewer.heading(index, text=f"{column[0]}")
+
+        for row in self.data:
+            self.data_viewer.insert('', tkinter.END, values=row)
+
+    def __configure_options__(self):
+        info_label = ttk.Label(self.options_frame, text="Options")
+        info_label.grid(column=0, row=0)
+
+        delete_button = ttk.Button(self.options_frame, text="Delete Row")
+        delete_button.grid(column=0, row=3)
+        delete_button.bind("<ButtonPress-1>", lambda e: self.delete_row_from_database_viewer())
+
+        edit_button = ttk.Button(self.options_frame, text="Edit Row")
+        edit_button.grid(column=0, row=2)
+        edit_button.bind("<ButtonPress-1>", lambda e: self.edit_row_from_database_viewer())
+
+        add_button = ttk.Button(self.options_frame, text=f"Add {self.object_message.title()}")
+        add_button.grid(column=0, row=1)
+        add_button.bind("<ButtonPress-1>", lambda e: self.show_window_editor()) 
+
+        save_button = ttk.Button(self.options_frame, text="Save")
+        save_button.grid(column=0, row=4)
+        save_button.bind("<ButtonPress-1>", lambda e: database.save_data())
+        
+    #Deletes a selected row from the database viewer
+    def delete_row_from_database_viewer(self):
+        try:
+            row_selection = self.data_viewer.focus()
+            self.data_viewer.delete(row_selection)
+        except:
+            print("No row selected!")
+            # show_error_window("No row selected!") FIXME add error window class
+
+    #Gets the row to be edditted and shows the meal window editor to edit the row
+    def edit_row_from_database_viewer(self):
+        row_info = self.data_viewer.item(self.data_viewer.focus())
+        self.show_window_editor(row_info["values"])
+
+    #Shows a window to add meals to the database, requires the root of the window and the data viewer for the meal table
+    def show_window_editor(self, row_data=None):
+        window = tkinter.Toplevel()
+
+        if (row_data == None or len(row_data) == 0):
+            window.title(f"Add a {self.object_message.title()}")
+        else:
+            window.title(f"Edit a {self.object_message.title()}")
+
+        window.minsize(window.winfo_reqwidth() + 50, window.winfo_reqwidth() + 50)
+        #Centers and sizes window (width x height + xPos + yPos)
+        window.geometry(f"200x200+{math.floor(self.root.winfo_screenwidth()/2 - 100)}+{math.floor(self.root.winfo_screenheight()/2 - 100)}")
+
+        entries = []
+        labels = []
+
+        for index, column in enumerate(self.columns): #TODO have entries be type based on the column
+            column_name = column[0]
+            type = column[1]
+
+            label = ttk.Label(window, text=f'{column_name}:')
+            label.grid(column=0, row=index)
+
+            if (type == "TEXT"):
+                entry = ttk.Entry(window)
+                entry.grid(column=1, row=index)
+                entries.append(entry)
+                labels.append(label)
+            elif (column_name == "ID"):
+                id_label = ttk.Label(window, text="Automatically Generated")
+                id_label.grid(column=1, row=index)
+
+        if (row_data == None or len(row_data) == 0):
+            add_button = ttk.Button(window, text=f"Add {self.object_message}")
+            add_button.bind("<ButtonPress-1>", lambda e: (self.modify_row_database(labels, entries), self.refresh_data_viewer(), window.destroy()))
+        else:
+            add_button = ttk.Button(window, text=f"Save {self.object_message}")
+            add_button.bind("<ButtonPress-1>", lambda e: (self.add_row_database(entries), self.refresh_data_viewer(), window.destroy()))
+
+        add_button.grid(column=1, sticky="S")
+            
+    def add_row_database(self, labels, entries): #TODO error catching
+        """Adds a row to the database and then refreshes the database viewer
+    
+        Arguements: labels (the label associated with the entry)
+                    entries (entries used for user input)
+        """
+        meal = []
+
+        for index, entry in enumerate(entries):
+            text = entry.get()
+            if (text == ''):
+                text = "None"
+
+            meal.append((labels[index].cget("text").replace(":", ""), text))
+
+        database.add_row_to_table(self.table_name, meal)
+
+    def modify_row_database(self, labels, entries): #TODO error catching
+        """Modifies and existing row in the database
+        
+        Arguements: labels (the label associated with the entry)
+                    entries (entries used for user input)
+        """
+        meal = []
+
+        for index, entry in enumerate(entries):
+            text = entry.get()
+            if (text == ''):
+                text = "None"
+
+            meal.append((labels[index].cget("text").replace(":", ""), text))
+
+        #FIXME modify data using the database library
+            
+    #Refreshed data viewer widget
+    def refresh_data_viewer(self):
+        print("Refreshing data viewer!") #FIXME
